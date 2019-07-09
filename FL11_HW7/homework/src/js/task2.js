04de5ae3f920ecbbb2d2b9497ce38ecc73135545
Liudmyla Zahorui <liudmyla.zahorui@gmail.com>
2019-07-09 21:51:16 +0300

    HW7 : Code cleanup

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -1,55 +1,55 @@
-// Your code goes here
 let wantPlay = confirm('Do you want to play a game?');
 const slotIncrease = 4;
 const prizeGrow = 2;
 const prizeShrink = 2;
 
 if (wantPlay) {
-  let playAgain = false;
-
-  do {
-
-  let slotCount = 8;
-  let maxprize = 100;
-  let prize = 0;
-  let totalattempts = 3;
-  let wantContinue = false
+    let playAgain = false;
 
     do {
 
-     let correctnumber = Math.ceil(Math.random() * slotCount);
-     let currentprize = maxprize;
-     let userwon = false;
-     wantContinue = false;
-    
-     for (let i=0;i<totalattempts;i++) {
-        let choosen = parseInt(prompt('Choose roulette pocket number: \n Attempts left: ' + (totalattempts - i) 
-				+ '\n Total prize: ' + prize 
-				+ '\n Possible prize on current attempt: ' + currentprize));
-        if (choosen === correctnumber) {
-            userwon = true
-            break;
-        }
-        currentprize = currentprize / prizeShrink;
-     } 
+        let slotCount = 8;
+        let maxprize = 100;
+        let prize = 0;
+        let totalattempts = 3;
+        let wantContinue = false
 
-     if (userwon) {
-        prize = prize + currentprize;
-        wantContinue = confirm('Congratulation, you won!   Your prize is: ' + prize + ' $. Do you want to continue ?') 	
-        if (wantContinue) {
-          maxprize = maxprize * prizeGrow;
-          slotCount += slotIncrease;
-        }
-     }
+        do {
 
-  } while (wantContinue)
+            let correctnumber = Math.ceil(Math.random() * slotCount);
+            let currentprize = maxprize;
+            let userwon = false;
+            wantContinue = false;
 
-  alert('Thank you for your participation. Your prize is: ' + prize + ' $ ')
-  playAgain = confirm('Do you want to play again ?');
+            for (let i = 0; i < totalattempts; i++) {
+                let choosen = parseInt(prompt('Choose roulette pocket number: \n Attempts left: ' 
+                    + (totalattempts - i) +
+                    '\n Total prize: ' + prize +
+                    '\n Possible prize on current attempt: ' + currentprize));
+                if (choosen === correctnumber) {
+                    userwon = true
+                    break;
+                }
+                currentprize = currentprize / prizeShrink;
+            }
 
- } while (playAgain)
+            if (userwon) {
+                prize = prize + currentprize;
+                wantContinue = confirm('Congratulation, you won!   Your prize is: ' 
+                + prize + ' $. Do you want to continue ?')
+                if (wantContinue) {
+                    maxprize = maxprize * prizeGrow;
+                    slotCount += slotIncrease;
+                }
+            }
+
+        } while (wantContinue)
+
+        alert('Thank you for your participation. Your prize is: ' + prize + ' $ ')
+        playAgain = confirm('Do you want to play again ?');
+
+    } while (playAgain)
 
 } else {
-  alert('You did not become a billionaire, but can.');
+    alert('You did not become a billionaire, but can.');
 }
-
